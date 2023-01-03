7e8a512ba9785ee3056e18baf8145e95435bcaea
Phung Thanh Ngan <95693269+ptn25@users.noreply.github.com>
2023-01-04 04:09:32 +0700

    fix

--- asm/main.js
+++ asm/main.js
@@ -1,17 +1,30 @@
 var express = require('express')
+const hbs = require ('hbs')
 const { findProductByName, insertNewProduct, getAllProduct, updateProduct, findProductById, deleteProductById } = require('./databaseHandler')
 var app = express()
 
-app.set('view engine','hbs')
-app.use(express.urlencoded({extended:true}))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/search', async(req,res)=>{
-    const searchName = req.body.txtName
-    const searchResult = await findProductByName(searchName)
-    res.render('view', {results:searchResult})
+hbs.registerHelper('color', function(price)
+{
+    if (price > 50)
+    {
+        return 'green'
+    }
+    else
+    {
+        return 'blue'
+    }
 })
 
-app.post('/edit',async(req,res)=>{
+app.post('/search', async (req, res) => {
+    const searchName = req.body.txtName
+    const searchResult = await findProductByName(searchName)
+    res.render('view', { results: searchResult })
+})
+
+app.post('/edit', async (req, res) => {
     const id = req.body.id
     const name = req.body.txtName
     const price = req.body.txtPrice
@@ -21,54 +34,56 @@ app.post('/edit',async(req,res)=>{
     res.redirect('/view')
 })
 
-app.get('/edit', async(req,res)=>{
+app.get('/edit', async (req, res) => {
     const id = req.query.id
     const productToEdit = await findProductById(id)
-    res.render("edit", {product:productToEdit})
+    res.render("edit", { product: productToEdit })
 })
 
-app.get('/delete',async (req,res)=>{
+app.get('/delete', async (req, res) => {
     const id = req.query.id
     await deleteProductById(id)
     res.redirect('/view')
 })
 
-app.get('/view',async (req,res)=>{
+app.get('/view', async (req, res) => {
     let results = await getAllProduct()
-    res.render('view',{'results':results})
+    res.render('view', { 'results': results })
 })
 
-app.post('/new',async (req,res)=>{
+app.post('/new', async (req, res) => {
     let name = req.body.txtName
     let price = req.body.txtPrice
     let pictureURL = req.body.txtPic
     let category = req.body.txtCategory
     let newProduct = {
-        name : name,
-        price: Number.parseFloat(price) ,
+        name: name,
+        price: Number.parseFloat(price),
         picture: pictureURL,
         category: category
     }
-    // if(name.length < 5)
-    // {    
-    //     // res.status(400).send({message:"Content cannot be empty!"});
-    //     // return;
-    //     alert("Name must be longer than 5 characters! Please Try Again!");
-    // }else
-    await insertNewProduct(newProduct)
-    res.redirect('/view')
+    if (name.length < 5) {
+        res.render('newProduct', { alert_name: true })
+    }
+    else if (category.length < 1) {
+        res.render('newProduct', { alert_category: true })
+    }
+    else {
+        await insertNewProduct(newProduct)
+        res.redirect('/view')
+    }
 })
 
-app.get('/new',(req,res)=>{
+app.get('/new', (req, res) => {
     res.render('newProduct')
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
+
+
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT)
 console.log("Server is up!")
-
-
